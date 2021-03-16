54c881ab265098e8c5cd83b31319ee243343826d
Mwe-coder <el.rich56@yahoo.com>
2021-03-16 04:11:44 +0300

    First commit

---
+++ myfirstproject/myfirstapp/views.py
@@ -0,0 +1,122 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .forms import *
+
+# Create your views here.
+def error_404_view(request,exception):
+  return render(render,'404.html')
+
+
+def myfunctioncall(request):
+ return HttpResponse("Hello World! I love God. Thank you Lord for Jas")
+
+def myfunctionabout(request):
+ return HttpResponse("About response")
+
+def add(request, x, y):
+ return HttpResponse(x+y)
+
+def intro(request,name, age):
+ # To generate a JsonResponse, you need to define a dictionary
+ mydictionary = {
+  "name": name,
+  "age":age
+ }
+
+ return JsonResponse(mydictionary)
+
+def myfirstpage(request):
+ return render(request,"index.html")
+
+def mysecondpage(request):
+ return render(request,"second.html")
+
+def mythirdpage(request):
+ var ="Hello World!"
+ greeting = "Hey, how are you?"
+ fruits = ["apple", "mango", "banana"] 
+ num1, num2 = 13,5
+ ans = num1 > num2
+ #print(ans)
+ mydictionary = {
+  "var":var,
+  "msg":greeting,
+  "myfruits": fruits,
+  "a": num1,
+  "b":num2,
+  "ans":ans
+ }
+
+ return render(request,"third.html", context = mydictionary)
+ 
+def myimagepage(request):
+ return render(request, "imagepage.html")
+
+def myimagepage2(request):
+ return render(request, "imagepage2.html")
+
+def myimagepage3(request):
+ return render (request, "imagepage3.html")
+
+def myimagepage4(request):
+ return render (request, "imagepage4.html")
+
+def myimagepage5(request, imagename):
+ myimagename= str(imagename);
+ myimagename= myimagename.lower();
+ print(myimagename)
+ if myimagename=='django':
+  var=True
+ elif myimagename=='python':
+  var=False
+
+ mydictionary={
+  "var":var
+ }
+ return render (request, "imagepage5.html", context=mydictionary)
+def myform(request):
+ return render(request, 'myform.html')
+
+def submitmyform(request):
+ mydictionary={
+  "var1": request.POST['mytext'],
+  "var2": request.POST['mytextarea'],
+  "method":request.method
+ }
+ return JsonResponse(mydictionary)
+
+def myform2(request):
+ if request.method=="POST":
+  form = FeedbackForm(request.POST)
+
+  if form.is_valid():
+   title=request.POST['title']
+   subject=request.POST['subject']
+   mydictionary = {
+    'form':FeedbackForm()
+   }
+   
+   if title != title.upper():
+    mydictionary["error"]=True
+    mydictionary["errormsg"]= "Title should be in Capital"
+    return render(request, 'myform2.html', context=mydictionary)
+
+   else:
+    mydictionary["success"]= True
+    mydictionary["successmsg"]= "Form Submitted"
+    return render(request,'myform2.html',context=mydictionary)
+
+  # else:
+  #  mydictionary={
+  #   'form':form
+  #  }
+  #  return render(request,'myform2.html',context=mydictionary)
+  
+
+ elif request.method=="GET":
+  form= FeedbackForm() # FeedbackForm(None)
+  mydictionary={
+   "form":form
+  }
+  return render(request,'myform2.html',context=mydictionary)
+
